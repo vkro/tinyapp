02e68e3fe62d56bc1b048545d0c8156db1ef2a3f
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-29 04:22:04 +0000

    redirect all shortURL requests to its longURL

--- express_server.js
+++ express_server.js
@@ -29,7 +29,6 @@ app.get('/urls', (req, res) => {
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  console.log(urlDatabase);    // log the POST request body to the console
   res.redirect(`/urls/${shortURL}`);           // redirect to shortURL page
 });
 
@@ -45,6 +44,11 @@ app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+})
 
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
