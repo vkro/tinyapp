b181ae0a37a116a7868a2afbcf9ad5cc073f62f1
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-29 00:51:36 +0000

    add get request for /urls & pass URL data to template

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.ca'
@@ -9,6 +11,11 @@ const urlDatabase = {
 
 app.get('/', (request, response) => {
   response.send('Hello!');
+});
+
+app.get('/urls', (request, response) => {
+  let templateVars = { urls: urlDatabase};
+  response.render('urls_index', templateVars);
 });
 
 app.get('/urls.json', (request, response) => {
