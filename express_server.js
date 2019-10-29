41edca49cc3712b8275d045b2bc56a8761ad6f38
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-29 03:11:57 +0000

    add route that handles POST request from urls/new form

--- express_server.js
+++ express_server.js
@@ -20,13 +20,18 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.post('/urls', (req, res) => {
+  console.log(req.body);    // log the POST request body to the console
+  res.send('OK');           // respond with 'OK' (we will replace this)
+});
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
-})
+});
 
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
