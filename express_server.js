758d3b1a715988133078b62b6fe9fa1084b75fad
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-29 20:43:03 +0000

    add delete route w/ redirect back to urls_index page

--- express_server.js
+++ express_server.js
@@ -45,6 +45,11 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  
+  res.redirect('/urls');
+});
+
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
