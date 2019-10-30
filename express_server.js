f33927368f36d5856b28c27641fcb0327d5e1e0f
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-30 00:03:47 +0000

    add POST route that updates URL resource on edit form submission

--- express_server.js
+++ express_server.js
@@ -40,14 +40,19 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
-app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render('urls_show', templateVars);
+app.post('/urls/:shortURL', (req, res) => {
+  urlDatabase[req.params.shortURL] = (req.body.newURL);
+  res.redirect(`/urls/${req.params.shortURL}`);
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
 });
 
 app.get('/u/:shortURL', (req, res) => {
