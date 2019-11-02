aaf7983267f1a6e8683a02c6d5487b62f99e8829
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-02 00:55:16 +0000

    cleanup

--- express_server.js
+++ express_server.js
@@ -28,6 +28,7 @@ const users = {};
 
 app.get('/', (req, res) => {
   const templateVars = { loginError: false };
+  
   if (!req.session.user_id) {                         // if nobody's logged in
     res.render('login', templateVars);                // send them to the login page
   } else {
@@ -35,8 +36,10 @@ app.get('/', (req, res) => {
   }
 });
 
+
 app.get('/register', (req, res) => {
   const templateVars = { registrationError: false };  // this will be used later in POST /register route
+  
   if (!req.session.user_id) {                         // if nobody's logged in
     res.render('register', templateVars);             // let them register
   } else {
@@ -46,6 +49,7 @@ app.get('/register', (req, res) => {
 
 app.get('/login', (req, res) => {
   const templateVars = { loginError: false };         // this will be used later in POST /login route
+  
   if (!req.session.user_id) {                         // if nobody's logged in
     res.render('login', templateVars);                // then let them log in
   } else {
@@ -53,10 +57,12 @@ app.get('/login', (req, res) => {
   }
 });
 
+
 app.get('/urls', (req, res) => {
   const currentUser = req.session.user_id;
   const filteredURLs = urlsForUser(currentUser, urlDatabase);  // filter urlDatabase for urls owned by currently logged-in user
   let templateVars = { urls: filteredURLs, user: users[currentUser] };         // ** NOTE TO SELF ** variables sent to an EJS template need to be sent inside an object, so that we can access the data w/ a key
+  
   if (!currentUser) {                                 // check if there's someone logged in
     res.status(403);                                  // if not, send 403 status code
   }
@@ -72,6 +78,7 @@ app.get('/urls.json', (req, res) => {
 app.get('/urls/new', (req, res) => {
   let templateVarsNew = { user: users[req.session.user_id] };
   let templateVarsLogin = { loginError: true };
+
   if (req.session.user_id) {                           // if someone's logged in
     res.render('urls_new', templateVarsNew);           // send them to the form to create a new tinyURL
   } else {
@@ -79,6 +86,7 @@ app.get('/urls/new', (req, res) => {
     res.render('login', templateVarsLogin);            // and show them the login page with a cue that they've made an error
   }
 });
+
 
 app.get('/urls/:shortURL', (req, res) => {
   let userID = req.session.user_id;
@@ -95,7 +103,7 @@ app.get('/urls/:shortURL', (req, res) => {
     } else {
       res.status(403);                                 // otherwise, user doesn't own shortURL, so send 403 status
     }
-    templateVars = { access: access, user: users[userID] };  // only include access status and userID if user doesn't have access to this shortURL
+    templateVars = { access: access, user: users[userID] };  // for these two cases, only send access status and userID to template
   }
   res.render('urls_show', templateVars);               // render shortURL page based on value of access variable
 });
@@ -103,7 +111,8 @@ app.get('/urls/:shortURL', (req, res) => {
 
 app.get('/u/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
-  if (urlDatabase[shortURL]) {                         // if the shortURL exists in the database
+
+  if (urlDatabase[shortURL]) {                         // if this shortURL exists in the database
     const longURL = urlDatabase[shortURL]['longURL'];
     res.redirect(longURL);                             // redirect to the associated longURL
   } else {
@@ -112,25 +121,29 @@ app.get('/u/:shortURL', (req, res) => {
   }
 });
 
+
 // POST ROUTES
 
-app.post('/urls', (req, res) => {            // create new shortURL
-  const shortURL = generateRandomString();
+
+app.post('/urls', (req, res) => {                      // create new shortURL
+  const shortURL = generateRandomString();             // assign a random 6-digit alpha-numeric string as the shortURL
   const user = req.session.user_id;
   const newURL = {};
   const date = new Date();
   const dateToString = date.toString();
-  if (user) {                                // if user is logged in
+
+  if (user) {                                          // if user is logged in
     newURL['longURL'] = req.body.longURL;
     newURL['userID'] = user;
     newURL['dateCreated'] = dateToString.substring(4, 15);
-    urlDatabase[shortURL] = newURL;          // add new entry to urlDatabase
-    res.redirect(`/urls/${shortURL}`);       // and redirect to new shortURL page
+    urlDatabase[shortURL] = newURL;                    // add new entry to urlDatabase
+    res.redirect(`/urls/${shortURL}`);                 // and redirect to new shortURL page
   } else {
-    res.status(403);                         // otherwise, send 403 status
-    res.redirect('/login');                  // and redirect to login page
+    res.status(403);                                   // otherwise, send 403 status
+    res.redirect('/login');                            // and redirect to login page
   }
 });
+
 
 app.post('/register', (req, res) => {
   const newUserID = generateRandomString();
@@ -138,43 +151,41 @@ app.post('/register', (req, res) => {
   const newPassword = bcrypt.hashSync(req.body.password, 10);
   const templateVars = { registrationError: false };
 
-  if (newEmail === "" || newPassword === "") {     // if either field is blank, send 400 code and redirect back to registration page
+  if (newEmail === "" || newPassword === "") {         // if email or password fields are blank, send 400 code and redirect back to registration page
     res.status(400);
     res.render('register', templateVars);
   } else if (emailAlreadyRegistered(newEmail, users, () => {
-    return true;
+    return true;                                       // check if email is already registered in the user database
   }
   )) {
-    templateVars.registrationError = true;        // if email already registered in database, render registration page with message letting user know
-    res.render('register', templateVars);
-  } else {                                        // otherwise, add new user to database and redirect to /urls
-    users[newUserID] = {
+    templateVars.registrationError = true;             // if email is already registered,
+    res.status(403);                                   // send 403 code
+    res.render('register', templateVars);              // and render registration page with error message
+  } else {
+    users[newUserID] = {                               // otherwise, add new user to database and redirect to /urls/new
       'email': newEmail,
       'password': newPassword
     };
     req.session.user_id = newUserID;
-    res.redirect(`/urls`);
+    res.redirect('/urls/new');
   }
 });
 
 app.post('/login', (req, res) => {
   const email = req.body.email;
-  const pw = req.body.password;
-  const userID = emailAlreadyRegistered(email, users, (user) => user);
-  const hashedPW = emailAlreadyRegistered(email, users, (user) => {
-    return users[user]['password'];
-  });
+  const userID = emailAlreadyRegistered(email, users, (userID) => userID);                  // if email is in user database, retrieve userID
+  const hashedPW = emailAlreadyRegistered(email, users, (user) => users[user]['password']); // if email is in user database, retreive hashed password
   const templateVars = { loginError: false };
 
-  if (userID) {
-    if (bcrypt.compareSync(pw, hashedPW)) {   // verify password with stored hashed password
+  if (userID) {                               // if the user exists in the database,
+    if (bcrypt.compareSync(req.body.password, hashedPW)) {   // verify submitted password against stored hashed password
       req.session.user_id = userID;           // set session cookie
-      res.redirect('/urls');                  // redirect to urls index
+      res.redirect('/urls');                  // and redirect to user's urls index
     }
   }
-  templateVars.loginError = true;             // if wrong email or password entered
-  res.status(403);                            // error code and
-  res.render('login', templateVars);          // go back to login page, displaying message cueing user that wrong login info has been entered
+  templateVars.loginError = true;             // if user doesn't exist yet, or wrong email or password entered
+  res.status(403);                            // send 403 status
+  res.render('login', templateVars);          // and show login page, with message cueing user that they've entered wrong login info
 });
 
 
@@ -182,25 +193,25 @@ app.post('/urls/:shortURL', (req, res) => {
   let currentUser = req.session.user_id;
   let shortURL = req.params.shortURL;
 
-  if (currentUser === whoseUrlIsThis(shortURL, urlDatabase)) {            // if the person logged in is the owner of this shortURL
-    urlDatabase[shortURL]['longURL'] = (req.body.newURL);    //let them edit it
-    res.redirect(`/urls/${shortURL}`);
+  if (currentUser === whoseUrlIsThis(shortURL, urlDatabase)) {           // if the person logged in is the owner of this shortURL
+    urlDatabase[shortURL]['longURL'] = req.body.newURL;                  // change the longURL stored at this shortURL in the database to their submitted url
+    res.redirect(`/urls/${shortURL}`);                                   // and redirect to the shortURL page
   } else {
-    res.sendStatus(403);                                    // otherwise, send 403 status code
+    res.sendStatus(403);                                                 // otherwise, send 403 status code
   }
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   let shortURL = req.params.shortURL;
-  if (req.session.user_id === whoseUrlIsThis(shortURL, urlDatabase)) {   // same thing happening here as in POST /urls/:shortURL (edit)
-    delete urlDatabase[req.params.shortURL];
+  if (req.session.user_id === whoseUrlIsThis(shortURL, urlDatabase)) {   // similar thing happening here as in POST /urls/:shortURL (edit)
+    delete urlDatabase[req.params.shortURL];                             // except this time delete the corresponding database entry
     res.redirect('/urls');
   } else {
     res.sendStatus(403);
   }
 });
 
-app.post('/logout', (req, res) => {
+app.post('/logout', (req, res) => {                                      // clear the session cookie and go back to login page
   req.session = null;
   res.redirect('login');
 });
